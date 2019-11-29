1143e67509fde90255712331ff56eeb0813fc726
Udit Pathak <pathak.udit816@gmail.com>
2019-11-30 03:55:37 +0530

    timer update

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("December 25, 2019 11:59:59").getTime();
+var countDownDate = new Date("December 21, 2019 06:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
