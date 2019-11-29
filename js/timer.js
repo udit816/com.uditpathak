293f3e280e5f8539b92aede7b5ceebbd64053d8e
Udit Pathak <pathak.udit816@gmail.com>
2019-11-29 19:03:24 +0530

    timr update

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 7, 2020 11:59:59").getTime();
+var countDownDate = new Date("December 25, 2019 11:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
