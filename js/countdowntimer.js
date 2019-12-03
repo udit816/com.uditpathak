bb7f7c1a74ba39e2134f9b2c6f6053a3e05f6081
Udit Pathak <pathak.udit816@gmail.com>
2019-12-03 14:58:43 +0530

    timer adjust

--- js/countdowntimer.js
+++ js/countdowntimer.js
@@ -3,21 +3,20 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Dec 31, 2019 00:00:00').getTime(),
+let countDown = new Date('Dec 31, 2019 23:59:59').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
-          distance = countDown - now;
+        distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
-
+        if (distance > 0) {
+            document.getElementById('days').innerText = Math.floor(distance / (day)),
+            document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+        } else {
+            clearInterval(x);
+        document.getElementById("success").innerHTML = "Happy New year!";
+        }
+        
     }, second)
